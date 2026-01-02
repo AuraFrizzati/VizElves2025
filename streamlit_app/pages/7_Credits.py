9e8c74a9eaea99ee1815f491b669b67553658d60
AuraFrizzati <frizzatia@gmail.com>
2026-01-02 19:31:25 +0000

    updated

--- streamlit_app/pages/7_Credits.py
+++ streamlit_app/pages/7_Credits.py
@@ -9,7 +9,7 @@ st.sidebar.header("Credits")
 st.markdown("## Challenge")
 st.markdown(
     """This Data Viz has been submitted to the 
-    [Data Lab Community’s Data Visualisation Competition 2025](https://thedatalab.com/data-visualisation-competition-2025/)
+    [Data Lab Community's Data Visualisation Competition 2025](https://thedatalab.com/data-visualisation-competition-2025/)
     """
 )
 
@@ -17,10 +17,10 @@ st.markdown(
 st.markdown("## Credits")
 st.markdown(
     """
-    * Created by Owen Craig Evans and Aura Frizzati 
+    * Created by [Aura Frizzati](https://www.linkedin.com/in/aura-frizzati/) and [Owen Craig Evans](https://www.linkedin.com/in/owen-evans-pcqi/) 
     * Data has been downloaded from the [UK Co-Benefits Atlas](https://ukcobenefitsatlas.net/), 
     an open source resource that models 11 net-zero co-benefits based on actions recommended by 
     the Climate Change Committee (CCC) in its Seventh Carbon Budget (2025) across 45,000 
-    communities and regions within the UK.
+    communities and regions within the UK
     """
 )
