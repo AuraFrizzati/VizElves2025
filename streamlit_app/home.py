7de11b1672eacaae3c7be7663d7ae0cd1450edd7
AuraFrizzati <frizzatia@gmail.com>
2026-01-02 19:24:03 +0000

    home updated

--- streamlit_app/home.py
+++ streamlit_app/home.py
@@ -28,12 +28,13 @@ st.markdown(
     """
 - [Cardiff Overview](./Cardiff_Overview): In this page we used the provided demographic data to illustrate the diversity in how Cardiff residents 
     live and the potential "green rewards" available to different areas. We also explored the total co-benefits expected by Cardiff by implementing Net-Zero green initiatives.
-- [Co-Benefits Analysis](./Co-Benefits_Analysis): This section focuses on the expected value generated in Cardiff through the Net Zero transition. 
+- [Co-Benefits Analysis](./Co-Benefits_Analysis): This section focusses on the expected value generated in Cardiff through the Net Zero transition. 
    We analysed each available co-benefit subcategory, exploring both the distribution of data by neighbourhood and the overall projections through 2050
-- [Social Deprivation Analysis](./Social_Deprivation_Analysis)
-- [Data Quality](./Data_Quality)
-- [Definitions & Methods](./Definitions_and_Methods)
-- [Credits](./Credits)
+- [Social Deprivation Analysis](./Social_Deprivation_Analysis): This section evaluates how the expected Net Zero Co-Benefits are distributed across Cardiff's socioeconomic landscape, 
+   using the Welsh Index of Multiple Deprivation (WIMD) quintile 2025 data 
+- [Data Quality](./Data_Quality): we show the completeness of data by co-benefit type (some co-benefits were excluded since mainly contained null values)
+- [Definitions & Methods](./Definitions_and_Methods): definitions of technical terms and methods used in the dashboard
+- [Credits](./Credits): links to the competition page and dashboard's authors
     """
 )
 
