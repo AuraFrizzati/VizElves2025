8406641970281843fc35d34ba89553db013a483c
AuraFrizzati <frizzatia@gmail.com>
2026-01-02 19:29:43 +0000

    updated

--- streamlit_app/home.py
+++ streamlit_app/home.py
@@ -18,23 +18,25 @@ with st.expander('About this Data Viz'):
              cobenefits to population]
                 """)
     
-st.header("Audience")
-st.markdown("""
-            We believe this dashboard could support policy-makers working on planning and implementing Environmental Net-Zero policies in Cardiff
-            """)
+
 
 st.markdown("## Index")
 st.markdown(
     """
-- [Cardiff Overview](./Cardiff_Overview): In this page we used the provided demographic data to illustrate the diversity in how Cardiff residents 
+- **[Cardiff Overview](./Cardiff_Overview)**: In this page we used the provided demographic data to illustrate the diversity in how Cardiff residents 
     live and the potential "green rewards" available to different areas. We also explored the total co-benefits expected by Cardiff by implementing Net-Zero green initiatives.
-- [Co-Benefits Analysis](./Co-Benefits_Analysis): This section focusses on the expected value generated in Cardiff through the Net Zero transition. 
+- **[Co-Benefits Analysis](./Co-Benefits_Analysis)**: This section focusses on the expected value generated in Cardiff through the Net Zero transition. 
    We analysed each available co-benefit subcategory, exploring both the distribution of data by neighbourhood and the overall projections through 2050
-- [Social Deprivation Analysis](./Social_Deprivation_Analysis): This section evaluates how the expected Net Zero Co-Benefits are distributed across Cardiff's socioeconomic landscape, 
+- **[Social Deprivation Analysis](./Social_Deprivation_Analysis)**: This section evaluates how the expected Net Zero Co-Benefits are distributed across Cardiff's socioeconomic landscape, 
    using the Welsh Index of Multiple Deprivation (WIMD) quintile 2025 data 
-- [Data Quality](./Data_Quality): we show the completeness of data by co-benefit type (some co-benefits were excluded since mainly contained null values)
-- [Definitions & Methods](./Definitions_and_Methods): definitions of technical terms and methods used in the dashboard
-- [Credits](./Credits): links to the competition page and dashboard's authors
+- **[Data Quality](./Data_Quality)**: we show the completeness of data by co-benefit type (some co-benefits were excluded since mainly contained null values)
+- **[Definitions & Methods](./Definitions_and_Methods)**: definitions of technical terms and methods used in the dashboard
+- **[Credits](./Credits)**: links to the competition page, main data collection and dashboard's authors
     """
 )
 
+st.header("## Audience")
+st.markdown("""
+           * This dashboard could support **Cardiff policy-makers** working on planning and implementing Environmental Net-Zero policies
+           * It can also be used by **Cardiff citizens** curious to know more about the impact of net-zero policies in their living area
+            """)
